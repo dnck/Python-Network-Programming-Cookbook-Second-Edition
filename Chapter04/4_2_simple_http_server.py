057781d5f0053c3e8fbaaab29c7d06af2dd49850
dnck <dnck@users.noreply.github.com>
2019-01-24 12:27:53 +0100

    correct chapter04-recipe2 to use bytes literal on line 29

--- Chapter04/4_2_simple_http_server.py
+++ Chapter04/4_2_simple_http_server.py
@@ -19,23 +19,23 @@ DEFAULT_PORT = 8800
 
 class RequestHandler(BaseHTTPRequestHandler):
     """ Custom request handler"""
-    
+
     def do_GET(self):
         """ Handler for the GET requests """
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         # Send the message to browser
-        self.wfile.write("Hello from server!")
+        self.wfile.write(b"Hello from server!")
         return
-    
+
 
 class CustomHTTPServer(HTTPServer):
     "A custom HTTP server"
     def __init__(self, host, port):
         server_address = (host, port)
         HTTPServer.__init__(self, server_address, RequestHandler)
-        
+
 
 def run_server(port):
     try:
@@ -52,6 +52,6 @@ def run_server(port):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simple HTTP Server Example')
     parser.add_argument('--port', action="store", dest="port", type=int, default=DEFAULT_PORT)
-    given_args = parser.parse_args() 
+    given_args = parser.parse_args()
     port = given_args.port
     run_server(port)
